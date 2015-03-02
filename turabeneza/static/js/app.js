d2f08d94df3b8cf9a495686de17d4e553733f138
RUGANO Allan Stockman <kuristockyrugano@gmail.com>
2015-03-02 20:25:45 +0200

    Stabilized layout

--- turabeneza/static/js/app.js
+++ turabeneza/static/js/app.js
@@ -1,57 +1,112 @@
+'use strict';
 
-$(function () {
-    var data = [];
-    var raba = {{timess|safe}};
-    for (var i = 0; i < raba.length; i+=2) {
-        data.push([parseInt(moment(raba[i+1]).format("x")), raba[i]] );
-    };
-    $('#container').highcharts({
-        chart: {
-            type: 'spline'
-        },
-        title: {
-            text: 'Amount set aside for Children'
-        },
-        subtitle: {
-            text: 'Irregular time data collected'
-        },
-        xAxis: {
-            type: 'datetime',
-            dateTimeLabelFormats: { // don't display the dummy year
-                month: '%b \'%y',
-                day: '%e. %b',
-                // year: '%Y. %b'
-            },
-            title: {
-                text: 'Date'
-            }
-        },
-        yAxis: {
-            title: {
-                text: 'Montant in Fb'
-            },
-            min: 0
-        },
-        tooltip: {
-            headerFormat: '<b>{series.name}</b><br>',
-            pointFormat: '{point.x:%e. %b}: {point.y} Fb'
-        },
+var myapp = angular.module('myapp', ["highcharts-ng"]);
 
-        plotOptions: {
-            spline: {
-                marker: {
-                    enabled: true
-                }
-            }
-        },
-        series: [
-            {
-                name:'Montant',
-                data: data
-            }]
-    });
+myapp.controller('myctrl', function ($scope) {
+
+  $scope.chartTypes = [
+    {"id": "line", "title": "Line"},
+    {"id": "spline", "title": "Smooth line"},
+    {"id": "area", "title": "Area"},
+    {"id": "areaspline", "title": "Smooth area"},
+    {"id": "column", "title": "Column"},
+    {"id": "bar", "title": "Bar"},
+    {"id": "pie", "title": "Pie"},
+    {"id": "scatter", "title": "Scatter"}
+  ];
+
+  $scope.dashStyles = [
+    {"id": "Solid", "title": "Solid"},
+    {"id": "ShortDash", "title": "ShortDash"},
+    {"id": "ShortDot", "title": "ShortDot"},
+    {"id": "ShortDashDot", "title": "ShortDashDot"},
+    {"id": "ShortDashDotDot", "title": "ShortDashDotDot"},
+    {"id": "Dot", "title": "Dot"},
+    {"id": "Dash", "title": "Dash"},
+    {"id": "LongDash", "title": "LongDash"},
+    {"id": "DashDot", "title": "DashDot"},
+    {"id": "LongDashDot", "title": "LongDashDot"},
+    {"id": "LongDashDotDot", "title": "LongDashDotDot"}
+  ];
+
+  $scope.chartSeries = [
+    {"name": "Some data 1", "data": [1, 2, 4, 7, 3]},
+    {"name": "Some data 3", "data": [3, 1, null, 5, 2], connectNulls: true},
+    {"name": "Some data 2", "data": [5, 2, 2, 3, 5], type: "column"},
+    {"name": "My Super Column", "data": [1, 1, 2, 3, 2], type: "column"}
+  ];
+
+  $scope.chartStack = [
+    {"id": '', "title": "No"},
+    {"id": "normal", "title": "Normal"},
+    {"id": "percent", "title": "Percent"}
+  ];
+
+  $scope.addPoints = function () {
+    var seriesArray = $scope.chartConfig.series;
+    var rndIdx = Math.floor(Math.random() * seriesArray.length);
+    seriesArray[rndIdx].data = seriesArray[rndIdx].data.concat([1, 10, 20])
+  };
+
+  $scope.addSeries = function () {
+    var rnd = []
+    for (var i = 0; i < 10; i++) {
+      rnd.push(Math.floor(Math.random() * 20) + 1)
+    }
+    $scope.chartConfig.series.push({
+      data: rnd
+    })
+  }
+
+  $scope.removeRandomSeries = function () {
+    var seriesArray = $scope.chartConfig.series;
+    var rndIdx = Math.floor(Math.random() * seriesArray.length);
+    seriesArray.splice(rndIdx, 1)
+  }
+
+  $scope.removeSeries = function (id) {
+    var seriesArray = $scope.chartConfig.series;
+    seriesArray.splice(id, 1)
+  }
+
+  $scope.toggleHighCharts = function () {
+    this.chartConfig.useHighStocks = !this.chartConfig.useHighStocks
+  }
+
+  $scope.replaceAllSeries = function () {
+    var data = [
+      { name: "first", data: [10] },
+      { name: "second", data: [3] },
+      { name: "third", data: [13] }
+    ];
+    $scope.chartConfig.series = data;
+  };
+
+  $scope.chartConfig = {
+    options: {
+      chart: {
+        type: 'areaspline'
+      },
+      plotOptions: {
+        series: {
+          stacking: ''
+        }
+      }
+    },
+    series: $scope.chartSeries,
+    title: {
+      text: 'Hello'
+    },
+    credits: {
+      enabled: true
+    },
+    loading: false,
+    size: {}
+  }
+
+  $scope.reflow = function () {
+    $scope.$broadcast('highchartsng.reflow');
+  };
+
+
 });
-$(function() {
-            var data = {{responses_pie_json|safe}} ;
-            $("#output").pivotUI(data)
-        });
