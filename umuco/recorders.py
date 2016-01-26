3df4a4d8ee90bffeca9e56d26ae34879fc52984d
RUGANO Allan Stockman <kuristockyrugano@gmail.com>
2016-01-26 14:54:06 +0200

    stabilized

--- umuco/recorders.py
+++ umuco/recorders.py
@@ -16,17 +16,21 @@ def check_number_of_values(args):
         if len(args['text'].split('#')) < 6:
             args['valide'] = False
             args['info_to_contact'] = "Erreur. Vous avez envoye peu de valeurs. Veuillez reenvoyer le message corrige."
-        if len(args['text'].split('#')) > 6:
-            args['valide'] = False
-            args['info_to_contact'] = "Erreur. Vous avez envoye beaucoup de valeurs. Veuillez reenvoyer le message corrige."
+        if len(args['text'].split('#'))  == 7:
+            args['number_of_values'] = 7
+            args['valide'] = True
+            args['info_to_contact'] = "Le nombre de valeurs envoye est correct."
         if len(args['text'].split('#')) == 6:
             args['valide'] = True
             args['info_to_contact'] = "Le nombre de valeurs envoye est correct."
+        if len(args['text'].split('#')) > 7:
+            args['valide'] = False
+            args['info_to_contact'] = "Erreur. Vous avez envoye peu de valeurs. Veuillez reenvoyer le message corrige."
 
 def check_password(args):
     ''' This function checks if the person who sent this message sent a valid password.
     The password to use is saved in localsettings'''
-
+    # import ipdb; ipdb.set_trace()
     the_expected_password = getattr(settings,'PASSWORD','')
     the_sent_password = args['text'].split('#')[1]
 
@@ -72,25 +76,28 @@ def check_colline(args):
 
 def check_phone(args):
     ''' This function cheks if the phone number is well written '''
-    contact_phone_number = args['text'].split('#')[4]
-    contact_phone_number_no_space = contact_phone_number.replace(" ", "")
-    expression = r'^(\+?(257)?)((62)|(79)|(71)|(76)|(75)|(72))([0-9]{6})$'
-    # import ipdb; ipdb.set_trace()
-    print(contact_phone_number_no_space)
-    if re.search(expression, contact_phone_number_no_space) is None:
-        #The phone number is not well written
-        args['valide'] = False
-        args['info_to_contact'] = "Erreur. Le numero de telephone envoye n est pas bien ecrit."
-    else:
-        args['valide'] = True
-        args['info_to_contact'] = "Le numero de telephone du superviseur est bien ecrit."
+    contact_phone_numbers = args['text'].split('#')[4:-1]
+    for contact_phone_number in contact_phone_numbers:
+        contact_phone_number_no_space = contact_phone_number.replace(" ", "")
+        expression = r'^(\+?(257)?)((62)|(79)|(71)|(76)|(75)|(72))([0-9]{6})$'
+        print(contact_phone_number_no_space)
+        if re.search(expression, contact_phone_number_no_space) is None:
+            #The phone number is not well written
+            args['valide'] = (args['valide'] and False)
+            args['info_to_contact'] = "Erreur. Le numero de telephone envoye n est pas bien ecrit."
+        elif PhoneModel.objects.filter(number='+257'+contact_phone_number_no_space).count() > 0:
+            args['valide'] = (args['valide'] and False)
+            args['info_to_contact'] = "Erreur. Le numero de telephone existe deja pour un autre groupe."
+        else:
+            args['valide'] = (args['valide'] and True)
+            args['info_to_contact'] = "Le numero de telephone du superviseur est bien ecrit."
 
 def check_report_day(args):
     ''' This function checks if the day on wich this new group will report is valid '''
     expression = r'^[1-7]{1}$'
 
     #Reporting_day is the day on which the group whose contact is in process of being registered reports
-    reporting_day =	args['text'].split('#')[5]
+    reporting_day =	args['text'].split('#')[-1]
 
     if re.search(expression, reporting_day) is None:
         args['valide'] = False
@@ -122,6 +129,7 @@ def record_reporter(args):
 
     #Let's check if this person sent a valid phone number
     check_phone(args)
+    # import ipdb; ipdb.set_trace()
     if not args['valide']:
         return
 
@@ -132,26 +140,32 @@ def record_reporter(args):
 
     the_commune = args['text'].split('#')[2].upper()
     the_colline = args['text'].split('#')[3].upper()
-    the_phone_number = args['text'].split('#')[4]
-    the_meetting_day = args['text'].split('#')[5]
-
-    the_phone_number = the_phone_number.replace(" ", "")
-    if len(the_phone_number) == 8:
-        the_phone_number = "+257"+the_phone_number
-    if len(the_phone_number) == 11:
-        the_phone_number = "+"+the_phone_number
+    the_phone_numbers = args['text'].split('#')[4:-1]
+    the_meetting_day = args['text'].split('#')[-1]
 
     the_concerned_group, created = NawenuzeGroup.objects.get_or_create(commune = the_commune, colline = the_colline)
     the_concerned_group = the_concerned_group
     the_concerned_group.day_of_meeting = the_meetting_day
     the_concerned_group.save()
 
-    the_phone_object, created = PhoneModel.objects.get_or_create(number = the_phone_number, group = the_concerned_group)
+    numbers = []
+    for the_phone_number in the_phone_numbers:
+        the_phone_number = the_phone_number.replace(" ", "")
+        if len(the_phone_number) == 8:
+            the_phone_number = "+257"+the_phone_number
+        if len(the_phone_number) == 11:
+            the_phone_number = "+"+the_phone_number
+
+        the_phone_object, created = PhoneModel.objects.get_or_create(number = the_phone_number, group = the_concerned_group)
+        numbers.append(the_phone_number)
     args['valide'] = True
     args['info_to_contact'] = "Tu as enregistres le groupe {0} dans la commune {1} pour donner les rapports tous les {2}.".format(the_colline, the_commune, days[int(the_meetting_day)])
+
     url = "https://app.rapidpro.io/api/v1/broadcasts.json"
-    the_message_to_send = "Tu as ete enregistres comme rapporteur du groupe {0} dans la commune {1}. Nous attendons les rapports tous les {2}".format(the_colline, the_commune, days[int(the_meetting_day)])
-    data = {"urns": ['tel:' + the_phone_number],"text": the_message_to_send}
-    requests.post(url, headers={'Content-type': 'application/json', 'Authorization': 'Token %s' % settings.TOKEN}, data = json.dumps(data))
+    for i in numbers:
+        print 'envoi a %s' % (i)
+        the_message_to_send = "Tu as ete enregistres comme rapporteur du groupe {0} dans la commune {1}. Nous attendons les rapports tous les {2}".format(the_colline, the_commune, days[int(the_meetting_day)])
+        data = {"urns": ['tel:' + i],"text": the_message_to_send}
+        requests.post(url, headers={'Content-type': 'application/json', 'Authorization': 'Token %s' % settings.TOKEN}, data = json.dumps(data))
     args['envoye'] = the_message_to_send
 
